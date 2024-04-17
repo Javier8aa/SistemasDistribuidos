80fa84ec3b88b4a23a95772a9a199ef98d5cb6f5
Javier8aa <160834962+Javier8aa@users.noreply.github.com>
2024-04-17 13:40:00 +0200

    Add files via upload

---
+++ PSSDD_lBermejo_jlOchoa/src/main/java/com/example/PSSDD_lBermejo_jlOchoa/PssddLBermejoJlOchoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PSSDD_lBermejo_jlOchoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PssddLBermejoJlOchoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PssddLBermejoJlOchoaApplication.class, args);
+	}
+
+}
---
+++ PSSDD_lBermejo_jlOchoa/src/test/java/com/example/PSSDD_lBermejo_jlOchoa/PssddLBermejoJlOchoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSSDD_lBermejo_jlOchoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PssddLBermejoJlOchoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
