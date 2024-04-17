c7aa9430ac9a7e493c6e4214e01f8e09b2ad40b2
Javier8aa <jl.ochoa.2020@alumnos.urjc.es>
2024-04-17 14:12:45 +0200

    Services

--- PSSDD_lBermejo_jlOchoa/src/main/java/com/example/PSSDD_lBermejo_jlOchoa/Services/AisleService.java
+++ PSSDD_lBermejo_jlOchoa/src/main/java/com/example/PSSDD_lBermejo_jlOchoa/Services/AisleService.java
@@ -1,9 +1,62 @@
 package com.example.PSSDD_lBermejo_jlOchoa.Services;
 
+import com.example.PSSDD_lBermejo_jlOchoa.Entities.Aisle;
 import com.example.PSSDD_lBermejo_jlOchoa.Repositories.AisleRepository;
 import com.example.PSSDD_lBermejo_jlOchoa.Repositories.FoodRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AisleService (AisleRepository aisleRepository, FoodRepository foodRepository){
-    this.foodRepository = foodRepository;
-    this.aisleRepository = aisleRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AisleService {
+
+    private AisleRepository aisleRepository;
+
+    @Autowired
+    public AisleService( AisleRepository aisleRepository) {
+        this.aisleRepository = aisleRepository;
+    }
+
+    public void saveAisle ( Aisle aisle) {
+        aisleRepository.save(aisle);
+    }
+
+    public Aisle update( Long id, @Valid UpdateAisleForm form ) {
+        var aisle = form.update(id, aisleRepository);
+        return aisle
+    }
+
+    public void deleteById ( Long id ) {
+        aisleRepository.deleteById(id);
+    }
+
+    public List<Aisle> findAllAisles ( ) {
+        List<Aisle> aisles = aisleRepository.findAll();
+        return aisles;
+    }
+
+    public Optional<Aisle> findById (Long id ) {
+        Optional<Aisle> aisle = aisleRepository.findById(id);
+        return aisle;
+    }
+
+    public void saveAll ( List<Aisle> aisles) {
+        aisleRepository.saveAll(aisles);
+    }
+
+    public AisleService(AisleRepository aisleRepository, FoodRepository foodRepository){
+        this.foodRepository = foodRepository;
+        this.aisleRepository = aisleRepository;
+    }
+    public void addFoodToAisle (Long aisleId, Long foodId) throws NotFoundException{
+        //Complete after mapping
+        Aisle aisle = aisleRepository.findyId(aisleId)
+                .orElseThrow(() -> new NotFoundException("Aisle not found"));
+        Food food = foodRepository.findById(foodId)
+                .orElseThrow( () -> new NotFoundException("Food not found"));
+    }
+
 }
